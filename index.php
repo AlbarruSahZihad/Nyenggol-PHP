72e8ea5216e91e246b4192743b3aff6060b77786
AlbarruSahZihad <zihadkevin7@gmail.com>
2026-06-10 12:19:38 +0700

    Belajar -PHP DASAR-

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+echo "Hello World!";
+echo "<br>";
+
+// Case sensitive: nama dan NAMA adalah variabel berbeda
+$nama = "ZIHAD";
+$NAMA = "ALBARRUSAH";
+
+echo "Variabel nama: " . $nama . "<br>";
+echo "Variabel NAMA: " . $NAMA . "<br>";
+?>
