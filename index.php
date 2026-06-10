38daf30f7441ae73df541aa1ff308100ddd3b1b0
AlbarruSahZihad <zihadkevin7@gmail.com>
2026-06-10 12:33:16 +0700

    pembaruan -PHP DASAR-

--- index.php
+++ index.php
@@ -1,11 +1,16 @@
-<?php
-echo "Hello World!";
-echo "<br>";
-
-// Case sensitive: nama dan NAMA adalah variabel berbeda
-$nama = "ZIHAD";
-$NAMA = "ALBARRUSAH";
-
-echo "Variabel nama: " . $nama . "<br>";
-echo "Variabel NAMA: " . $NAMA . "<br>";
-?>
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <title>HTML dan PHP</title>
+</head>
+<body>
+    <h1>Belajar PHP</h1>
+    <?php
+        $mahasiswa = "Albarrusah Zihad S Pane";
+        $npm = "243510691";
+        echo "<p>Nama: " . $mahasiswa . "</p>";
+        echo "<p>NPM: " . $npm . "</p>";
+    ?>
+</body>
+</html>
