f3d8ff65d14639fa4d5ccb9e1955bb974be2c985
AlbarruSahZihad <zihadkevin7@gmail.com>
2026-06-10 12:39:43 +0700

    pembaruan2 -PHP DASAR-

--- index.php
+++ index.php
@@ -2,15 +2,26 @@
 <html lang="id">
 <head>
     <meta charset="UTF-8">
-    <title>HTML dan PHP</title>
+    <title>Form PHP</title>
 </head>
 <body>
-    <h1>Belajar PHP</h1>
+    <h2>Form Login</h2>
+    <form method="POST" action="">
+        <label>Nama Pengguna:</label><br>
+        <input type="text" name="username"><br><br>
+        <label>Kata Sandi:</label><br>
+        <input type="password" name="password"><br><br>
+        <button type="submit" name="submit">Login</button>
+    </form>
+
     <?php
-        $mahasiswa = "Albarrusah Zihad S Pane";
-        $npm = "243510691";
-        echo "<p>Nama: " . $mahasiswa . "</p>";
-        echo "<p>NPM: " . $npm . "</p>";
+    if (isset($_POST['submit'])) {
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        echo "<h3>Data yang dimasukkan:</h3>";
+        echo "<p>Username: " . htmlspecialchars($username) . "</p>";
+        echo "<p>Password: " . htmlspecialchars($password) . "</p>";
+    }
     ?>
 </body>
 </html>
