c79ef2c87d9f8536a58ddc781a9f9862e52f6117
AlbarruSahZihad <zihadkevin7@gmail.com>
2026-06-11 20:02:02 +0700

    Penambahan -sikit-

--- index.php
+++ index.php
@@ -3,24 +3,33 @@
 <head>
     <meta charset="UTF-8">
     <title>Form PHP</title>
+    <style>
+        body{
+            margin: 10vh;
+        }
+    </style>
 </head>
 <body>
-    <h2>Form Login</h2>
-    <form method="POST" action="">
-        <label>Nama Pengguna:</label><br>
+    <h2>Form Login Mahasiswa</h2>
+    <form method="POST" action="" style="align-items: center; ">
+        <label>Nama Lengkap:</label><br>
         <input type="text" name="username"><br><br>
-        <label>Kata Sandi:</label><br>
-        <input type="password" name="password"><br><br>
-        <button type="submit" name="submit">Login</button>
+        <label>NPM:</label><br>
+        <input type="number" name="npm"><br><br>
+        <label>Alamat:</label><br>
+        <input type="text" name="alamat"><br><br>
+        <button type="submit" name="submit">Submit</button>
     </form>
 
     <?php
     if (isset($_POST['submit'])) {
         $username = $_POST['username'];
-        $password = $_POST['password'];
+        $npm = $_POST['npm'];
+        $alamat = $_POST['alamat'];
         echo "<h3>Data yang dimasukkan:</h3>";
-        echo "<p>Username: " . htmlspecialchars($username) . "</p>";
-        echo "<p>Password: " . htmlspecialchars($password) . "</p>";
+        echo "<p>username: " . htmlspecialchars($username) . "</p>";
+        echo "<p>npm: " . htmlspecialchars($npm) . "</p>";
+        echo "<p>alamat: " . htmlspecialchars($alamat) . "</p>";
     }
     ?>
 </body>
